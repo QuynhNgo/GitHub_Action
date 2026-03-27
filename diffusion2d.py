86b59a32f35171d1f2093c3b8628afaf83d195ae
Quynh Ngo <ngoqh@visus.uni-stuttgart.de>
2026-03-27 11:03:17 +0100

    x = 2 added to diffusion2d initialize_domain

--- diffusion2d.py
+++ diffusion2d.py
@@ -45,6 +45,7 @@ class SolveDiffusion2D:
         self.nx = int(w / dx)
         self.ny = int(h / dy)
         self.bug = 0
+        x = 2
         for member in [self.w, self.h, self.dx, self.dy]:
             assert (
                 type(member) == float
